c472e37ce85ab9c28905ac8a7b0a9c4e654ed6a4
Adam Stirtan <adamstirtan@gmail.com>
2020-10-20 12:46:45 -0400

    Basic SCV parsing complete

---
+++ SCVPreview/SCVPreview/SCV/SCV.cs
@@ -0,0 +1,14 @@
+﻿namespace SCVPreview.SCV
+{
+    public class SCV
+    {
+        public SCV(string line)
+        {
+            var split = line.Split("|");
+
+            Identifier = split[0];
+        }
+
+        public string Identifier { get; set; }
+    }
+}
